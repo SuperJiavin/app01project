767b1670d5f6875185c02f8aebb1f3204f01a5b7
andy <3376165056@qq.com>
2023-09-03 22:45:45 +0800

    init project

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+const webpackConfig = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+};
+
+module.exports = webpackConfig;
